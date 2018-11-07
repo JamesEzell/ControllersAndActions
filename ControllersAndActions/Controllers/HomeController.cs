025691df2d745f6a47f878d08144a74ced5d3ee5
James Ezell <Ezell_JamesP40@outlook.com>
2018-11-06 20:20:55 -0800

    11/6

--- ControllersAndActions/Controllers/HomeController.cs
+++ ControllersAndActions/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Text;
+using ControllersAndActions.Infrastructure;
 
 namespace ControllersAndActions.Controllers
 {
@@ -7,13 +8,6 @@ namespace ControllersAndActions.Controllers
     {
         public ViewResult Index() => View("SimpleForm");
 
-        public void ReceiveForm(string name, string city)
-        {
-            Response.StatusCode = 200;
-            Response.ContentType = "text/html";
-            byte[] content = Encoding.ASCII.GetBytes($"<html><body>{name} lives in {city}</body>");
-            Response.Body.WriteAsync(content, 0, content.Length);
-        }
-
+        public ViewResult ReceiveForm(string name, string city) =>  View("Result", $"{name} lives in {city}");
     }
 }
--- ControllersAndActions/Infrastructure/CustomHtmlResult.cs
+++ ControllersAndActions/Infrastructure/CustomHtmlResult.cs
@@ -1,11 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ControllersAndActions.Infrastructure
 {
-    public class CustomHtmlResult
+    public class CustomHtmlResult : IActionResult
     {
+        public string Content { get; set; }
+
+        public Task ExecuteResultAsync(ActionContext context)
+        {
+            context.HttpContext.Response.StatusCode = 200;
+            context.HttpContext.Response.ContentType = "text/html";
+            byte[] content = Encoding.ASCII.GetBytes(Content);
+            return context.HttpContext.Response.Body.WriteAsync(content, 0, content.Length);
+
+        }
     }
 }
